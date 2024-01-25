0f3a06b4863e6e4e31b5f9c665d10054f1d3e101
01star01ek <01star01ek@gmail.com>
2024-01-25 14:44:23 +0900

    [01star01ek] 메인화면 텍스트 변경

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>나의 첫번째 앱입니다.</Text>
+      <Text>멋진 앱을 만들거예요.</Text>
     </View>
   );
 }
